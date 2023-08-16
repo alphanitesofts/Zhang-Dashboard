f8c00ddb31b8f9a4164028112d45989e16539dab
Faraz Mahmood <faraz.m4765@gmail.com>
2023-08-16 20:52:42 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Body/Navbar";
+import NotificationBar from "./Components/Body/NotificationBar";
+import Sidebar from "./Components/Body/Sidebar";
+import Main from "./Components/Main";
+import Footer from "./Components/Body/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Navbar />
+      <Sidebar />
+      <NotificationBar />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Body/NotificationBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotificationBar = () => {
+  return (
+    <div>
+        <aside className="control-sidebar control-sidebar-dark">
+          <div className="p-3">
+              <h5>Title</h5>
+              <p>Sidebar content</p>
+          </div>
+      </aside>
+    </div>
+  )
+}
+
+export default NotificationBar
